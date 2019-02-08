d97c56906f1d09300b43e40dfb03733b6280a659
GuanlinWang <43226744+GuanlinWang@users.noreply.github.com>
2019-02-07 21:14:54 -0800

    Update Program1.cpp

--- Program1.cpp
+++ Program1.cpp
@@ -1,1 +1,22 @@
+#include<iostream>
+#include <fstream>
+using namespace std;
 
+struct  PERSON {
+    char  Name[20];
+    float Balance; };
+
+int main()
+{
+  ifstream file;
+  file.open("data.txt");
+  int counter=0;
+  string temp;
+  while(file)
+  {
+    getline(file,temp);
+    counter++;
+  }
+  file.close();
+  counter--;
+}
