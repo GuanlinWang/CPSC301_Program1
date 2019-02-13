b1ef692eed666c168163a362b6549c6fd16cb725
GuanlinWang <43226744+GuanlinWang@users.noreply.github.com>
2019-02-12 21:49:18 -0800

    Update Program1.cpp

--- Program1.cpp
+++ Program1.cpp
@@ -8,7 +8,9 @@ struct  PERSON {
     char  Name[20];
     float Balance; };
 void Display(PERSON P[], int size);
-
+void FindRichest(PERSON P[],int size);
+void Deposit(string CustName,PERSON P[],int size);
+void NewCopy(string fileName, PERSON P[], int size);
 int main()
 {
     ifstream file;
@@ -32,13 +34,18 @@ int main()
      for(int i =0;i <counter;i++)
     {
         file>>str1>>str2>>num;
-        fullName=str1+"  "+str2;
+        fullName=str1+" "+str2;
         strcpy(P[i].Name,fullName.c_str());
         P[i].Balance=num;
     }
     file.close();
     Display(P,counter);
-   
+    FindRichest(P,counter);
+    string custName;
+    cout<<"Enter you full name to deposit money: ";
+    getline(cin,custName);
+    Deposit(custName,P,counter);
+    NewCopy("data.txt",P,counter);
 }
 
 
@@ -51,4 +58,53 @@ void Display(PERSON P[], int size)
         cout.precision(2);
         cout<<"       "<<setw(24)<<left<<P[i].Name <<right<<fixed<<P[i].Balance<<endl;
     }
+    cout<<endl;
 }
+
+void FindRichest(PERSON P[],int size)
+{
+    float topBalance=P[0].Balance;
+    char richest[20];
+    for (int i=0;i<size;i++)
+    {
+        if(P[i+1].Balance>P[i].Balance)
+        {
+            topBalance=P[i+1].Balance;
+            strcpy(richest,P[i+1].Name);
+        }
+    }
+    
+    cout<<"The customer with maximum balance is "<<richest<<endl;
+}
+
+void Deposit(string CustName,PERSON P[],int size)
+{
+    float add;
+    cout<<CustName<<", how much would you like to deposit? ";
+    cin>>add;
+    for(int i=0;i<size;i++)
+    {
+        if (P[i].Name==CustName)
+        {
+            P[i].Balance+=add;
+            cout<<"Now your new Balance is "<<P[i].Balance<<endl;
+            break;
+        }
+    }
+    
+}
+void NewCopy(string fileName, PERSON P[], int size)
+{
+    fstream file;
+    file.open(fileName,fstream::out);
+    for (int i=0;i<size;i++)
+    {
+        file.precision(2);
+        file<<P[i].Name<<" "<<fixed<<P[i].Balance<<endl;
+        cout<<endl;
+    }
+   
+    
+}
+
+
